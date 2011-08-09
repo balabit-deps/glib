d8f3c576c2eb7b9d2199e49ae5ef825cad3cbacc
Tamas Pal <folti@balabit.hu>
2011-08-09 13:31:05 +0200

    Added OSX specific builder instructions.

--- autogen.sh
+++ autogen.sh
@@ -46,15 +46,21 @@ fi
 	DIE=1
 }
 
-if automake-1.7 --version < /dev/null > /dev/null 2>&1 ; then
-    AUTOMAKE=automake-1.7
-    ACLOCAL=aclocal-1.7
+set -x
+if [ "`uname -s`" = "Darwin" ];then
+	AUTOMAKE=automake
+	ACLOCAL=aclocal
 else
-	echo
-	echo "You must have automake 1.7.x installed to compile $PROJECT."
-	echo "Install the appropriate package for your distribution,"
-	echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
-	DIE=1
+    if automake-1.7 --version < /dev/null > /dev/null 2>&1 ; then
+        AUTOMAKE=automake-1.7
+        ACLOCAL=aclocal-1.7
+    else
+        echo
+        echo "You must have automake 1.7.x installed to compile $PROJECT."
+        echo "Install the appropriate package for your distribution,"
+        echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
+        DIE=1
+    fi
 fi
 
 if test "$DIE" -eq 1; then
